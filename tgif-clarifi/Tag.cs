745b0258e6d0e6ae52f8ac3054afd3e8cab7e3f8
Ben Ishay <shimon.ben-ishay@hpe.com>
2016-08-12 01:22:04 +0300

    adding c# solution

---
+++ tgif-clarifi/Tag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tgif_clarifi
+{
+    public class Tag
+    {
+        private readonly string name;
+        private readonly double probability;
+
+        Tag(string name, double probability)
+        {
+            this.name = name;
+            this.probability = probability;
+        }
+
+        /**
+         * Returns the name of the tag. The name will be in the language specified by the Locale passed
+         * to {@link RecognitionRequest#setLocale(java.util.Locale)}, or the application's default
+         * language if none was specified. Note that the name may consist of more than one word.
+         */
+        public string getName()
+        {
+            return name;
+        }
+
+        /** Returns a probability that this tag is associated with the input image. */
+        public double getProbability()
+        {
+            return probability;
+        }
+
+        public override string ToString()
+        {
+            return "[Tag " + name + ": " + probability + "]";
+        }
+    }
+}
